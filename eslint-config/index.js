b68f5d9a517daf0cce31c11cdb28dbfcaf7b203d
Mike Kreiser <kreiserm@nextcapital.com>
2025-02-04 14:25:47 -0600

    Rename eslint-config-js-base to eslint-config

---
+++ eslint-config/index.js
@@ -0,0 +1,42 @@
+module.exports = [
+  ...([
+    './base-rules/flat/best-practices',
+    './base-rules/flat/errors',
+    './base-rules/flat/node',
+    './base-rules/flat/style',
+    './base-rules/flat/variables',
+    './base-rules/flat/es6',
+    './base-rules/flat/imports',
+    './base-rules/flat/strict',
+  ].reduce((p, c) => p.concat(require(c)), [])), // eslint-disable-line global-require, import/no-dynamic-require
+  {
+    env: {
+      es6: true
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2016,
+        sourceType: 'module'
+      }
+    },
+    files: [
+      '**/*.{js,mjs,cjs,jsx}'
+    ],
+    ignores: [
+      '.git/',
+      '**/node_modules/',
+      '**/build/',
+      '**/dist/',
+      '**/ts-output/'
+    ]
+  },
+  {
+    files: [
+      '**/*.test.js',
+      '**/*.test.jsx'
+    ],
+    rules: {
+      'no-multi-assign': 'off'
+    }
+  }
+];
